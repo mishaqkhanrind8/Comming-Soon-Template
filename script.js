577e071155a389b113c702b9c0ca73da8ebca2b2
Muhammad Ishaq Khan <aonemobilesncomputers786@gmail.com>
2025-09-04 06:39:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('✅ Service Worker registered with scope:', reg.scope))
     .catch(err => console.error('❌ Service Worker registration failed:', err));
 }
+
+
+function fixHeight() {
+  document.querySelector('.container').style.height = window.innerHeight + 'px';
+}
+window.addEventListener('resize', fixHeight);
+window.addEventListener('load', fixHeight);
